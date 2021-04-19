23a9a99cd7e4fd880546d69fa6f8e63c814d7482
123revanth <81552258+123revanth@users.noreply.github.com>
2021-04-19 11:26:26 +0530

    In this file i created 5*5 2d array and took the input from user printed the array again and also found the number that the user entered in the array

---
+++ 2dArrays.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int arr[3][3];
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << arr[i][j]<<" ";
+		}
+		cout << endl;
+	}
+	cout << "________________________";
+	int finder;
+	cout << "enter the number you want to find:";
+	cin >> finder;
+
+	bool flag = false;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (arr[i][j] == finder) {
+				cout << finder << " found"<<endl;	
+				flag = true;
+			}
+		}
+	}
+	cout << flag;
+	return 0;
+}
